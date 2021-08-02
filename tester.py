4c0e492dce02eaf6dbc4831e0e21717d8d43c8bf
dhruv <kadamdhruv98@gmail.com>
2021-08-02 16:28:56 -0500

    Added a pause function, and added a way to save current days tracked time to JSON file for future refrence.

--- tester.py
+++ tester.py
@@ -1,17 +1,28 @@
 from datetime import datetime
+import json
 
 
 dictionary = {}
+now = datetime.now()
+x = "resume"
 
-while 1 == 1:
+dictionary["Entry date"] = now.strftime("%a:%d:%b:%Y")
+
+while x  == "resume":
     now = datetime.now()
     current_time = now.strftime("%I:%M:%S:%p")
     task = input("task: ")
     dictionary[task] = current_time
-
     if task == "exit":
         break
+    if task == "pause":
+        x = 0
+        x = input("To resume type resume, to exit type exit:")
 
-
+with open('myfile.json', 'w') as fp:
+    json.dump(dictionary, fp)
 for key, value in dictionary.items():
     print(key, ' : ', value)
+
+
+
