cf397a58ee3215a55461817a384307bcad252071
dhruv <kadamdhruv98@gmail.com>
2021-08-04 23:37:47 -0500

    Made the first entry keep track of what day it is, and also added comments. Also uses a While True loop instead of a 1 == 1 loop. -Dhruv Kadam

--- tester.py
+++ tester.py
@@ -1,40 +1,46 @@
 import json
 import time
-from collections import defaultdict
 from datetime import datetime
-result = defaultdict(int)
 x = datetime.now().strftime("%a:%d:%b:%Y")
- 
-dictionary = {} 
+ #adds current day to the dictionary
+dictionary = {x : "current_day",}
 
 
 def task(taskname):
-    start_time = time.time() 
+    start_time = time.time()
+    #there was an if statment here, but I realized that you only don't really need to get this output
     input("Type anything to stop counting time: ")
     end_time = time.time()
+    #gets time that passed
     time_passed = end_time - start_time
+    #searches for existing task
     if taskname in dictionary:
+#gets old task time
       oldtime = (dictionary.get(taskname))
+# prints the time spent on the current iteration of the task
       print(time_passed)
+#adds two times together
       time_passed += oldtime
     else:
+#stores time passed in dictonary the first time
         dictionary[x] = time_passed
+#prints time passed
         print(time_passed)
-
+#stores time passed again
     dictionary[x] = time_passed
 
-
-while 1 == 1:
+#inf loop
+while True:
     x = input("enter taskname here:")
-    if x == "stop":
+    if x == "stop" or "exit":
         break
     else:
         task(x)
-
+#prints log of todays activity
 for key, value in dictionary.items():
     print(key, ':', value)
-
-with open('myfile.json, "w"') as fp:
+#stores everyting to json
+with open('myfile.json', 'w') as fp:
     json.dump(dictionary, fp)
 
         
